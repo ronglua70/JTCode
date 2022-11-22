a964a5b18c4fc896820a8b9e5f43710bb5ef8203
quocbao <ba0kieu952000@gmail.com>
2022-11-22 15:08:46 +0700

    setting source

---
+++ jt-code/modules/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const HomePage = (props: Props) => {
+  return (
+    <div className='bk__HomePage'>HomePage</div>
+  )
+}
+
+export default HomePage
