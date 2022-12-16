a455322e7d7e12b37e4016a5c842a320b58f006d
quocbao <ba0kieu952000@gmail.com>
2022-12-16 16:41:18 +0700

    test link and custom something

--- jt-code/modules/HomePage/index.tsx
+++ jt-code/modules/HomePage/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
+
+import { useRouter } from 'next/router'
 
 type Props = {}
 
 const HomePage = (props: Props) => {
+  const router = useRouter();
+
+  const handleGoToContactPage = () => {
+    return router.push({pathname: '/contact', query: {name: 'bao'}});
+  }
+
   return (
-    <div className='bk__HomePage'>HomePage</div>
+    <>
+      <div className='bk__HomePage'>HomePage</div>
+
+      <ul>
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link as={'AboutUs'} href={'/aboutus'}>About us</Link></li>
+        <li><Link prefetch={false} href={'/contact'}>Contact</Link></li>
+      </ul>
+
+      <button onClick={() => handleGoToContactPage()}>go to page</button>
+    </>
   )
 }
 
---
+++ jt-code/pages/aboutus/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const AboutUsPage = (props: Props) => {
+  return (
+    <div>AboutUsPage</div>
+  )
+}
+
+export default AboutUsPage
